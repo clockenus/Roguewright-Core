6745db72f63af2b2b7b9b8ba733c40d5b00a61b0
clockenus <mrclocken@gmail.com>
2026-08-30 00:37:15 +0300

    2

--- src/main/java/com/clocken/roguewright/Roguewright.java
+++ src/main/java/com/clocken/roguewright/Roguewright.java
@@ -1,10 +1,25 @@
 package com.clocken.roguewright;
 
+import com.natamus.starterkit_common_fabric.functions.StarterGearFunctions;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class Roguewright implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        ServerLivingEntityEvents.ALLOW_DEATH.register((entity, source, number) -> {
+            if (entity instanceof PlayerEntity player) {
+                player.getInventory().clear();
+            }
+            return true;
+        });
+
+        ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> {
+            StarterGearFunctions.initStarterKitHandle(oldPlayer.getServerWorld(), newPlayer, oldPlayer.getServerWorld().getServer().getCommandSource());
+        });
     }
+
 }
--- src/main/java/com/clocken/roguewright/mixin/GameModeListMixin.java
+++ src/main/java/com/clocken/roguewright/mixin/GameModeListMixin.java
@@ -1,4 +1,18 @@
 package com.clocken.roguewright.mixin;
 
-public class GameModeListMixin {
+import net.minecraft.world.GameMode;
+import net.minecraft.world.GameModeList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(GameModeList.class)
+public abstract class GameModeListMixin {
+    @Redirect(
+            method = "<clinit>",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/GameModeList;of([Lnet/minecraft/world/GameMode;)Lnet/minecraft/world/GameModeList;")
+    )
+    private static GameModeList roguewright$redirectList(GameMode[] gameModes) {
+        return GameModeList.of(GameMode.ADVENTURE);
+    }
 }
