1e3dea275578ccd7dbe822a7eda649b7e1de9736
clockenus <mrclocken@gmail.com>
2026-08-31 23:50:32 +0300

    chages dahh

--- src/main/java/com/clocken/roguewright/Roguewright.java
+++ src/main/java/com/clocken/roguewright/Roguewright.java
@@ -2,17 +2,14 @@ package com.clocken.roguewright;
 
 import com.natamus.starterkit_common_fabric.functions.StarterGearFunctions;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
-import net.minecraft.entity.player.PlayerEntity;
 
 public class Roguewright implements ModInitializer {
 
     @Override
     public void onInitialize() {
         ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> {
-            StarterGearFunctions.initStarterKitHandle(oldPlayer.getServerWorld(), newPlayer, oldPlayer.getServerWorld().getServer().getCommandSource());
+            StarterGearFunctions.initStarterKitHandle(newPlayer.getServerWorld(), newPlayer, newPlayer.getServerWorld().getServer().getCommandSource());
         });
     }
-
 }
